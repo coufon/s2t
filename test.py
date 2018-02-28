f1665164976e81e21f2498256d7b56166800de58
eric <zhoufang@ucsd.edu>
2018-02-28 09:29:54 -0800

    update attention

--- test.py
+++ test.py
@@ -12,25 +12,14 @@ from pycocoevalcap.tokenizer.ptbtokenizer import PTBTokenizer
 from pycocoevalcap.bleu.bleu import Bleu
 from pycocoevalcap.meteor.meteor import Meteor
 
-from config import *
+from config.msrvtt_config import *
 from nets.obj_feat_att_net import VideoCaptionGenerator
-from utils import get_video_data, preProBuildWordVocab
+from utils.msrvtt_utils import get_video_data, preProBuildWordVocab
 
 
-def test(model_path='models/model-61', video_feat_path=video_feat_path):
-    train_data, test_data = get_video_data(video_data_path, video_feat_path, train_ratio=0.7)
-    test_videos = test_data['video_path'].values
-    test_captions = test_data['Description'].values
+def test(model_path='models/model-61'):
+    captions = get_video_data(video_data_path_train, video_feat_path_train)
     ixtoword = pd.Series(np.load('./data/ixtoword.npy').tolist())
-
-    test_videos_unique = list()
-    test_captions_list = list()
-    for (video, caption) in zip(test_videos, test_captions):
-        if len(test_videos_unique) == 0 or test_videos_unique[-1] != video:
-            test_videos_unique.append(video)
-            test_captions_list.append([caption])
-        else:
-            test_captions_list[-1].append(caption)
 
     model = VideoCaptionGenerator(
             dim_image=dim_image,
@@ -40,13 +29,13 @@ def test(model_path='models/model-61', video_feat_path=video_feat_path):
             batch_size=1,
             dim_obj_feats = dim_obj_feats,
             n_obj_feats = n_obj_feats,
-            encoder_max_sequence_length=encoder_step,
+            #encoder_max_sequence_length=encoder_step,
             decoder_max_sentence_length=decoder_step,
             bias_init_vector=None)
 
-    tf_loss, tf_video_mask, tf_obj_feats, tf_caption, tf_caption_mask, tf_probs, tf_generated_words, tf_generated_att = model.build_model(is_test=True)
+    _, tf_obj_feats, tf_video_mask, _, _, tf_generated_words, tf_generated_att = \
+        model.build_model(is_test=True)
     sess = tf.InteractiveSession()
-
     saver = tf.train.Saver()
     saver.restore(sess, model_path)
 
@@ -56,9 +45,9 @@ def test(model_path='models/model-61', video_feat_path=video_feat_path):
     RES = defaultdict(list)
     counter = 0
 
-    for (vid, caption) in zip(test_videos_unique, test_captions_list):
+    for vid, caption in captions.items():
         # Collect frames.
-        cap = cv2.VideoCapture(os.path.join(video_path, vid[:-4]))
+        cap = cv2.VideoCapture(os.path.join(video_path, vid+'.mp4'))
         frames = list()
         while True:
             ret, im = cap.read()
@@ -67,16 +56,16 @@ def test(model_path='models/model-61', video_feat_path=video_feat_path):
             frames.append(im)
 
         # Load meta data.
-        with open(os.path.join(meta_data_path, vid[:-4]+'.txt'), 'r') as f:
+        with open(os.path.join(meta_data_path_train, vid+'.mp4.txt'), 'r') as f:
             meta_data = json.load(f)
             all_feats = meta_data['features']
 
-        generated_sentence, generated_att = gen_sentence(
+        generated_sentence, generated_att, video_mask = gen_sentence(
             sess, tf_video_mask, tf_obj_feats, tf_generated_words, tf_generated_att, vid, ixtoword)
         #generated_sentence_test, weights = gen_sentence(
         #    sess, video_tf, video_mask_tf, caption_tf, vid, ixtoword, weights_tf, 0.3)
         generated_att = [att[:, 0, 0] for att in generated_att]
-        #print generated_att
+        print generated_att
 
         print vid, generated_sentence
         #plt.plot(generated_att)
@@ -87,7 +76,7 @@ def test(model_path='models/model-61', video_feat_path=video_feat_path):
         words = generated_sentence.split(' ')
         feats = list()
         for i, w in enumerate(words):
-            i_best_feat = np.argmax(generated_att[i])
+            i_best_feat = np.argmax(np.multiply(generated_att[i], video_mask[0]))
             print w, i_best_feat
             if i_best_feat < len(all_feats):
                 feat = all_feats[i_best_feat]   
@@ -138,7 +127,7 @@ def gen_sentence(sess, tf_video_mask, tf_obj_feats, tf_generated_words, tf_gener
     #current_feats_vals = map(lambda vid: np.load(os.path.join(video_feat_path, vid)), current_videos)
     # Object features.
     obj_feats = np.zeros((1, n_obj_feats, dim_obj_feats))
-    feat = np.load(os.path.join(video_obj_feat_path, vid))
+    feat = np.load(os.path.join(video_feat_path_train, vid+'.mp4.npy'))
     n_obj = min(n_obj_feats, feat.shape[0])
     obj_feats[0, :n_obj] = feat[:n_obj]
 
@@ -164,8 +153,8 @@ def gen_sentence(sess, tf_video_mask, tf_obj_feats, tf_generated_words, tf_gener
     generated_words = generated_words[:punctuation]
 
     generated_sentence = ' '.join(generated_words)
-    return generated_sentence, generated_att
+    return generated_sentence, generated_att, video_mask
 
 
 if __name__=="__main__":
-    test(model_path='models/model-799')
+    test(model_path='models_cp/model-999')
