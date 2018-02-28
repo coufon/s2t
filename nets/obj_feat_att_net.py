d5cb3017e7178625379d402736fa031e21007f49
eric <zhoufang@ucsd.edu>
2018-02-28 09:29:26 -0800

    update attention

--- nets/obj_feat_att_net.py
+++ nets/obj_feat_att_net.py
@@ -127,8 +127,9 @@ class VideoCaptionGenerator():
                 e_list = tf.stack([
                     tf.matmul(tf.tanh(tf.add(obj_emb_proj, state_proj)), self.obj_embed_att_w) \
                         for obj_emb_proj in obj_emb_projs], axis=0)
+                e_list = tf.exp(e_list)
                 e_list_mask = tf.multiply(tf.expand_dims(tf.transpose(video_mask), -1), e_list)
-                weights = tf.nn.softmax(e_list_mask, dim=0)
+                weights = e_list_mask/tf.reduce_sum(e_list_mask, axis=0)
                 generated_attention.append(weights)
 
                 emb_weighted = [tf.multiply(emb, tf.tile(weight, [1, self.dim_embed])) \
